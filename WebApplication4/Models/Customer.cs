a1cb06eed6e1b72fe61e4889d54773625583b16c
buyab_h9pfnco <buyab_h9pfnco@MOSHOPC>
2024-04-19 19:02:26 +0300

    Add project files.

---
+++ WebApplication4/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication4.Models
+{
+    public class Customer
+    {
+        public string name { get; set; }
+
+        public int id { get; set; }
+
+        public string email { get; set; }
+
+        public string phone { get; set; }
+
+
+
+    }
+}
